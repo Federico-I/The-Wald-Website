fcbc51cf31b306495223916d68538616e5f9dafd
Federico-I <threxos.48@gmail.com>
2026-01-13 22:05:11 +0000

    app-Navigation

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,7 +1,18 @@
 import react from 'react';
+import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <div>Navigation</div>
+    <ul>
+      <li>
+        <Link href="/cabins">Cabin</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+      <li>
+        <Link href="/account">Your Account</Link>
+      </li>
+    </ul>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import react from "react";
 import Link from "next/link";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
   return ( 
     <dib>
+      <Navigation />
       <h1>Welcome to the Wald!</h1>
       
       <Link href="/cabins">Get your Premium Cabin!</Link>
