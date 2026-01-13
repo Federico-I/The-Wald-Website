0f3ea589cc5478f63a9da3e29a7df54719837332
Federico-I <threxos.48@gmail.com>
2026-01-13 21:07:44 +0000

    LinkComp-home

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import react from "react";
+import Link from "next/link";
 
 export default function Home() {
   return ( 
     <dib>
       <h1>Welcome to the Wald!</h1>
-      <a href="/cabins">Get your Premium Cabin!</a>
+      
+      <Link href="/cabins">Get your Premium Cabin!</Link>
     </dib>
   );
 }
