df7f204792aa0f996b366d0ba02f73dfa5940c86
Federico-I <threxos.48@gmail.com>
2026-03-04 23:15:24 +0000

    servercomponent-datafetching

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 
-export default function page() {
+export default async function page() {
+  const res = await fetch("example.com");
+  const data = await res.json();
+  
   return (
     <div>
-      <h1>Cabins page</h1>;
+      <h1>Cabins page</h1>
+      <ul>{data.map(user=><li>{user.name}</li>)}</ul>
     </div>
   );
 }
