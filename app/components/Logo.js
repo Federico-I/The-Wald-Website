7c9a18923dce5061699f2a3995d71fd462edd386
Federico-I <threxos.48@gmail.com>
2026-03-04 23:04:47 +0000

    Logo.js

---
+++ app/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-4 z-10">
+      <img height="60" width="60" alt="The Wald" />
+      <span className="text-xl font-semibold text-primary-100">
+      The Wald
+      </span>
+    </a>
+  )
+}
