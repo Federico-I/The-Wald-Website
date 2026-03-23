0283476b538465f9b4c86528c30be41df88f95ec
Federico-I <threxos.48@gmail.com>
2026-03-23 19:30:31 +0000

    counterClient/serverSide

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -1,10 +1,17 @@
+"use client";
+
 import React from 'react';
 import { useState } from 'react';
 
-export default function Counter() {
+export default function Counter({ users }) {
   const [count, setCount] = useState(0);
 
-  return  <button onClick={ () => setCount((c) => c + 1) }>
-            {count}
-          </button>
+  console.log(users);
+
+  return  <div>
+            <p>There are {users.lenght} users</p>
+            <button onClick={ () => setCount((c) => c + 1) }>
+              {count}
+            </button>  
+  </div>
 };
