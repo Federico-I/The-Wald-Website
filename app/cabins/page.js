44ba010bb334225e80e1885edded403c7e336b7c
Federico-I <threxos.48@gmail.com>
2026-03-25 21:07:25 +0000

    clean-cabinPage

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import Navigation from '@/app/_components/Navigation';
-import Counter from '@/app/_components/Counter';
 
-export default async function page() {
-  const res = await fetch("example.com");
-  const data = await res.json();
+export default function page() {
   
   return (
     <div>
       <h1>Cabins page</h1>
 
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data}/>
     </div>
   );
 }
