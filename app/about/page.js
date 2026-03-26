7ff7d1c3aa91ab36604905f2a0ec49032344d58e
Federico-I <threxos.48@gmail.com>
2026-03-26 19:24:39 +0000

    setting-MetaData

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,9 @@
 import react from 'react';
 
+export const metadata = {
+  title: "About",
+};
+
 export default function About() {
   return (
     <h1>All about The Wald</h1>
--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+export const metadata = {
+  title: "Guest area",
+}
+
 export default function page() {
   return (
     <h3>Your Account</h3>
   )
-}
+};
