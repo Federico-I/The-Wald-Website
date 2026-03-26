aedcda897e91792b65e0dd9bfbf176dec648e269
Federico-I <threxos.48@gmail.com>
2026-03-26 19:07:10 +0000

    layout-background

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return <html> 
-    <body>
+    <body className="bg-blue-900 text-gray-50 min-h-screen">
       <header>
         <Logo />
         <Navigation />
