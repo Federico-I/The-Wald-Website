dcf9299ef7ed42bad03f389fe120146a8695d98e
Federico-I <threxos.48@gmail.com>
2026-03-31 19:26:51 +0100

    logo-image-atributes

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image src="/logo.png" height="60" width="60" alt="The Wald" />
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wald" /> */}
+      <Image src={logo} quality={100} height="60" width="60" alt="The Wald" />
       <span className="text-xl font-semibold text-primary-100">
       The Wald
       </span>
