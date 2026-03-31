c1432f8be00b09f4095580a29be32a143b63348c
Federico-I <threxos.48@gmail.com>
2026-03-31 19:47:44 +0100

    pageApp-logo-background-attributes

--- app/page.js
+++ app/page.js
@@ -1,13 +1,24 @@
 import react from "react";
 import Link from "next/link";
-import Navigation from "@/app/_components/Navigation";
+import Image from "next/image";
+import bg from "@/public/bg.png"
 
 export default function Home() {
   return ( 
-    <dib>
-      <h1>Welcome to the Wald!</h1>
-      
-      <Link href="/cabins">Get your Premium Cabin!</Link>
-    </dib>
+     <main className="mt-24">
+      <Image src={bg} fill className="object-cover" alt="Mountains and forests with two cabins" />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to Balance.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore retreat cabins
+        </Link>
+      </div>
+    </main>
   );
 }
