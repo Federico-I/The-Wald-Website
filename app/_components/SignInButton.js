97d004908869613876d164edaaa63297959c80d1
Federico-I <threxos.48@gmail.com>
2026-04-07 19:13:07 +0100

    navigation-layout-sideButton

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,3 +1,5 @@
+import react from "react";
+
 function SignInButton() {
   return (
     <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
--- app/account/layout.js
+++ app/account/layout.js
@@ -1,10 +1,11 @@
 import react from 'react';
+import Navigation from '@/app/_components/Navigation';
 
 export default function Layout({ children }) {
   return (
     <div className='grid grid-cols [16rem_1fr] h-full gap-12'>
-      <div>Naigation</div>
-      <div>{children}</div>
+      <Navigation />
+      <div className='py-1'>{children}</div>
     </div>
   );
 }
