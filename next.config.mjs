9e011a14d2ac4844ecc68c459a266d7907963f4d
Federico-I <threxos.48@gmail.com>
2026-04-09 19:13:28 +0100

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        port: "",
+        pathname: "/account123/",
+      }
+    ]
+  },
   /* config options here */
   reactCompiler: true,
 };
