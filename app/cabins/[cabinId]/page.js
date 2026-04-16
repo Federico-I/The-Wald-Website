c592c19970d9b5b42a636142c9ffbf1e7a2bc092
Federico-I <threxos.48@gmail.com>
2026-04-16 22:09:52 +0100

    generateStaticParams-cabinId

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,6 @@
 import react from "react";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import Image from "next/image";
 
 // PLACEHOLDER DATA
@@ -19,6 +19,14 @@ import Image from "next/image";
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
   return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({
+    cabinId: cabin.id,
+  }));
 }
 
 export default async function Page({ params }) {
