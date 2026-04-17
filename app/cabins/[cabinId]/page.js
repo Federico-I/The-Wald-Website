b759a01d0b6d8df311fe449f6b5fdf1efaecc889
Federico-I <threxos.48@gmail.com>
2026-04-17 20:54:42 +0100

    cabinId-useClient

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,7 +1,9 @@
+"use Client"
 import react from "react";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import Image from "next/image";
+import TextExpander from "@/app/_components/TextExpander";
 
 // PLACEHOLDER DATA
 {/*const cabin = {
@@ -50,7 +52,11 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>
+              {description}
+            </TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
