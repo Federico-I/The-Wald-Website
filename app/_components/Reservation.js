ef8815a395255e47952dc769041b4f69d6982fef
Federico-I <threxos.48@gmail.com>
2026-04-30 19:37:49 +0100

    Reservation-improve-fetch

---
+++ app/_components/Reservation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DateSelector from './DateSelector';
+import ReservationForm from './ReservationForm';
+
+function Reservation() {
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+      <DateSelector />
+      <ReservationForm />
+    </div>
+  );
+}
+
+export default Reservation;
