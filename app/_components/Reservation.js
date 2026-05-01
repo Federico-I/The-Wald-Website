32744e75666bd45b3d1380e3f4d7ecded769eae8
Federico-I <threxos.48@gmail.com>
2026-05-01 19:40:45 +0100

    reservation-cabin-bookingDatesPromiseAsync

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import DateSelector from './DateSelector';
 import ReservationForm from './ReservationForm';
+import { getBookedDatesByCabinId, getCabin, getSettings } from '../_lib/data-service';
 
-function Reservation() {
+async function Reservation() {
+
+  const [cabin, settings, bookedDates] = await Promise.all([getCabin(params.cabinId), getSettings(), getBookedDatesByCabinId(params.cabinId),]);
+
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector />
--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -37,11 +37,9 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }) {
 
-  {/*const cabin = await getCabin(params.cabinId);
-  const settings = await getSettings();
-  const bookedDates = await getBookedDatesByCabinId(); */}
-
-  const [cabin, settings, bookedDates] = await Promise.all([getCabin(params.cabinId), getSettings(), getBookedDatesByCabinId(params.cabinId),]);
+  const cabin = await getCabin(params.cabinId);
+  // const settings = await getSettings();
+  // const bookedDates = await getBookedDatesByCabinId();
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
