9da23aa24b1502a99416092f1186ebc437bf7f2e
Federico-I <threxos.48@gmail.com>
2026-05-06 20:29:45 +0100

    ReservationContext-ReservationProvider

--- app/_components/ReservationContext.js
+++ app/_components/ReservationContext.js
@@ -5,5 +5,16 @@ const ReservationContext = createContext();
 const initialState = { from: undefined, to: undefined };
 
 function ReservationProvider() {
-  const [range, setRagnge] = useState();
+  const [range, setRagnge] = useState(initialState);
+
+  return (
+    <ReservationContext.Provider value={{ range, setRagnge }}>
+      {children}
+    </ReservationContext.Provider>
+  );
+}
+
+function useReservation() {
+  const context = useContext(ReservationContext);
+  if(context === undefined) throw new Error ("Context was outside provider");
 }
