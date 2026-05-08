75eef9a2ec88b309895dc1c54d7e14dfbbfdae16
Federico-I <threxos.48@gmail.com>
2026-05-08 20:20:09 +0100

    setNew-Range-inForm

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -20,9 +20,14 @@ function ReservationForm({ cabin }) {
             src={user.image}
             alt={user.name}
           />
+
           <p>{user.name}</p>
         </div> */}
       </div>
+
+      <p>
+        {range.from} to {range.to}
+      </p>
 
       <form className='bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col'>
         <div className='space-y-2'>
